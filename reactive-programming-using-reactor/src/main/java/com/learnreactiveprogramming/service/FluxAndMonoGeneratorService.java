30c044f66a931a674e9ffe2f2313a2a9f575d6a6
abhiishekpawar <abhipawar213@gmail.com>
2024-02-02 20:11:10 +0530

    new

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -7,30 +7,47 @@ import java.util.List;
 
 public class FluxAndMonoGeneratorService {
 
-    public Flux<String> listOfNames() {
+    public Flux<String> namesFlux() {
         return Flux.fromIterable(List.of("Name1", "Name2", "Name3"));
     }
 
-    public Mono<String> name(){
+    public Mono<String> namesMono(){
         return Mono.just("Name1");
+    }
+
+    public Flux<String> namesFluxMap(){
+        return Flux.just("alex","james","mock")
+                .map(String::toUpperCase);
+    }
+
+    public Flux<String> namesFluxFilterMap(){
+        return Flux.just("alex","james","mock")
+                .filter(n -> n.length() > 4)
+                .map(String::toUpperCase);
     }
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
-        //1st way
-        fluxAndMonoGeneratorService.listOfNames()
-                .subscribe(name -> {
-                    System.out.println("name is "+ name);
-                });
+//        //1st way
+//        fluxAndMonoGeneratorService.namesFlux()
+//                .subscribe(name -> {
+//                    System.out.println("name is "+ name);
+//                });
 
         //2nd way
-        fluxAndMonoGeneratorService.listOfNames()
-                .subscribe(System.out::println);
+//        fluxAndMonoGeneratorService.namesFlux()
+//                .subscribe(System.out::println);
+//
+//        fluxAndMonoGeneratorService.namesMono()
+//                .subscribe(name -> {
+//                    System.out.println("only name is "+name);
+//                });
 
-        fluxAndMonoGeneratorService.name()
-                .subscribe(name -> {
-                    System.out.println("only name is "+name);
-                });
+//        fluxAndMonoGeneratorService.namesFluxMap()
+//                .subscribe(System.out::println);
+
+        fluxAndMonoGeneratorService.namesFluxFilterMap()
+                .subscribe(System.out::println);
     }
 
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,54 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+
+    @Mock
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    @Test
+    void namesFlux(){
+        Flux<String> stringFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Name1")
+                .expectNext("Name2")
+                .expectNext("Name3")
+                .verifyComplete();
+
+    }
+    @Test
+    void namesMono(){
+        Mono<String> stringMono = fluxAndMonoGeneratorService.namesMono();
+
+        StepVerifier.create(stringMono)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+    @Test
+    void namesFluxMap(){
+        Flux<String> stringFlux = fluxAndMonoGeneratorService.namesFluxMap();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("ALEX")
+                .expectNextCount(3)
+                .expectComplete();
+    }
+    @Test
+    void namesFluxFilterMap(){
+        Flux<String> stringFlux = fluxAndMonoGeneratorService.namesFluxFilterMap();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(1)
+                .expectComplete();
+
+    }
+
 
 }
