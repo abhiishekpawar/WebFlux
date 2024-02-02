89f6697c162f7b8a4819657e4d55b5f265dc4cc1
abhiishekpawar <abhipawar213@gmail.com>
2024-02-02 18:53:00 +0530

    initial commit

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,36 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> listOfNames() {
+        return Flux.fromIterable(List.of("Name1", "Name2", "Name3"));
+    }
+
+    public Mono<String> name(){
+        return Mono.just("Name1");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        //1st way
+        fluxAndMonoGeneratorService.listOfNames()
+                .subscribe(name -> {
+                    System.out.println("name is "+ name);
+                });
+
+        //2nd way
+        fluxAndMonoGeneratorService.listOfNames()
+                .subscribe(System.out::println);
+
+        fluxAndMonoGeneratorService.name()
+                .subscribe(name -> {
+                    System.out.println("only name is "+name);
+                });
+    }
+
 }
